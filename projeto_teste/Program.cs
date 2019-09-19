929c10f1f7e160ef88cbf3b05eb43a26ec299d10
Joao Vitor <joaovitor24960@gmail.com>
2019-09-19 09:44:46 -0300

    Acrescentado uma mensagem de bom dia

--- projeto_teste/Program.cs
+++ projeto_teste/Program.cs
@@ -7,6 +7,8 @@ namespace projeto_teste
         static void Main(string[] args)
         {
             Console.WriteLine("Olá Mundo!");
+            Console.WriteLine("Bom dia!");
+            Console.ReadLine();
         }
     }
 }
